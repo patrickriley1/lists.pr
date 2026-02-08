9877c535f13b0feec289a908aea5a1d787594157
Patrick Riley <righthandriley32@gmail.com>
2026-02-08 01:58:06 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div>
+      <div className='header'>
+        <h1>lists.pr</h1>
+        <div className='verticalLineSmall'></div>
+      </div>
+      <div className='body'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App

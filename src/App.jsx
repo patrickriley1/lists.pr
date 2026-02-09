0b50582a905c974dd76e5e7c15b070d2250a4403
Patrick Riley <righthandriley32@gmail.com>
2026-02-08 19:58:52 -0500

    redirect fix again

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
   const clientID = "52ef8393bb03454a8d33998beacb0927";
-  const redirectURI = "https://lists-pr.vercel.app/";
+  const redirectURI = "https://lists-pr.vercel.app";
   const authEndpoint = "https://accounts.spotify.com/authorize";
   const responseType = "token";
+
+  const [token, setToken] = useState("");
 
   return (
     <div>
@@ -16,7 +18,6 @@ function App() {
       </div>
       <div className='body'>
         <a href={`${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}`}>Login to Spotify</a>
-
       </div>
     </div>
   )

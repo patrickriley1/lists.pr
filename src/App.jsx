083edf650f8f0afa4bd7c96230766b6d7df3e71b
Patrick Riley <righthandriley32@gmail.com>
2026-02-26 11:47:15 -0500

    sonica name change

--- src/App.jsx
+++ src/App.jsx
@@ -985,7 +985,7 @@ function App() {
   return (
     <div>
       <div className="header">
-        <h1>lists.pr</h1>
+        <h1>sonica</h1>
         <div className="verticalLineSmall"></div>
         <nav className="topNav">
           <Link className={location.pathname === "/" ? "navLink active" : "navLink"} to="/">
